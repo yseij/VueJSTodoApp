4bdb7a7dcd3043ec16f6fa613fb03de753554ee5
yseij <r0790156@student.thomasmore.be>
2022-02-15 20:39:46 +0100

    Pagina's en routes instellen

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
       <v-divider></v-divider>
 
       <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -30,7 +30,7 @@
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
@@ -40,8 +40,8 @@ export default {
   data: () => ({
     drawer: null,
     items: [
-      { title: "Todo", icon: "mdi-view-dashboard" },
-      { title: "About", icon: "mdi-help-box" },
+      { title: "Todo", icon: "mdi-format-list-checks", to: "/" },
+      { title: "About", icon: "mdi-help-box", to: "/about" },
     ],
   }),
 };
