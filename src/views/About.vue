3d0b1e52450bead6532daa10ff487a897a505e8c
yseij <r0790156@student.thomasmore.be>
2022-02-16 16:32:19 +0100

    Pagina's gereed maken

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About veutify Todo</h1>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi nobis
+      labore ea dolorem autem, nulla commodi libero quia. Assumenda voluptas,
+      dicta labore et veniam a qui. Expedita accusamus blanditiis nemo placeat
+      nobis voluptatum enim provident, molestiae quaerat quia repellat, esse
+      autem tempora nostrum. Voluptatem id quam facilis doloremque optio quo.
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi nobis
+      labore ea dolorem autem, nulla commodi libero quia. Assumenda voluptas,
+      dicta labore et veniam a qui. Expedita accusamus blanditiis nemo placeat
+      nobis voluptatum enim provident, molestiae quaerat quia repellat, esse
+      autem tempora nostrum. Voluptatem id quam facilis doloremque optio quo.
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi nobis
+      labore ea dolorem autem, nulla commodi libero quia. Assumenda voluptas,
+      dicta labore et veniam a qui. Expedita accusamus blanditiis nemo placeat
+      nobis voluptatum enim provident, molestiae quaerat quia repellat, esse
+      autem tempora nostrum. Voluptatem id quam facilis doloremque optio quo.
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi nobis
+      labore ea dolorem autem, nulla commodi libero quia. Assumenda voluptas,
+      dicta labore et veniam a qui. Expedita accusamus blanditiis nemo placeat
+      nobis voluptatum enim provident, molestiae quaerat quia repellat, esse
+      autem tempora nostrum. Voluptatem id quam facilis doloremque optio quo.
+    </p>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home pa-6">
+    <h1>Todo Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>
