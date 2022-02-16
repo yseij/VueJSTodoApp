233fe1ac0df42a311992886a5e88dcb0d2d4925d
yseij <r0790156@student.thomasmore.be>
2022-02-16 16:57:22 +0100

    Vervang afbeelding en titel

--- src/App.vue
+++ src/App.vue
@@ -23,13 +23,7 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      app
-      color="primary"
-      dark
-      src="https://picsum.photos/1920/1080?random"
-      prominent
-    >
+    <v-app-bar app color="primary" dark src="mountains.jpg" prominent>
       <template v-slot:img="{ props }">
         <v-img
           v-bind="props"
@@ -37,9 +31,9 @@
         ></v-img>
       </template>
 
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-app-bar-title>Title</v-app-bar-title>
+      <v-app-bar-title>Veutify Todo</v-app-bar-title>
 
       <v-spacer></v-spacer>
 
