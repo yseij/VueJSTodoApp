bd9467f00a935f5eb66a52fa8f1e9775871983ea
yseij <r0790156@student.thomasmore.be>
2022-02-16 19:26:09 +0100

    Taak markeren als voltooid

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,14 +2,20 @@
   <div class="home">
     <v-list flat class="pt-0">
       <div v-for="task in tasks" :key="task.id">
-        <v-list-item>
-          <template v-slot:default="{ active }">
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{ 'blue lighten-5': task.done }"
+        >
+          <template v-slot:default>
             <v-list-item-action>
-              <v-checkbox :input-value="active"></v-checkbox>
+              <v-checkbox :input-value="task.done"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title>{{ task.title }}</v-list-item-title>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through': task.done }"
+                >{{ task.title }}</v-list-item-title
+              >
             </v-list-item-content>
           </template>
         </v-list-item>
@@ -28,17 +34,26 @@ export default {
         {
           id: 1,
           title: "Wake up",
+          done: false,
         },
         {
           id: 2,
           title: "Get bananas",
+          done: false,
         },
         {
           id: 3,
           title: "Eat bananas",
+          done: false,
         },
       ],
     };
+  },
+  methods: {
+    doneTask(id) {
+      let task = this.tasks.filter((task) => task.id === id)[0];
+      task.done = !task.done;
+    },
   },
 };
 </script>
