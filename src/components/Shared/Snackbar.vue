ec380a0409f7a062f12980ff236fb9af5490a094
yseij <r0790156@student.thomasmore.be>
2022-02-17 16:51:13 +0100

    Maak de tekst van de snackbar dynamisch

--- src/components/Shared/Snackbar.vue
+++ src/components/Shared/Snackbar.vue
@@ -1,6 +1,6 @@
 <template>
   <v-snackbar v-model="$store.state.snackbar.show">
-    {{ text }}
+    {{ $store.state.snackbar.text }}
 
     <template v-slot:action="{ attrs }">
       <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
@@ -11,9 +11,5 @@
 </template>
 
 <script>
-export default {
-  data: () => ({
-    text: `Task added`,
-  }),
-};
+export default {};
 </script>
