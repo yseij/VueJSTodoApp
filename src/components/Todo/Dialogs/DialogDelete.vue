cca149d391e4a35fdebbd3816e7c46ab8471e270
yseij <r0790156@student.thomasmore.be>
2022-02-17 17:38:31 +0100

    De taak verwijderen wanneer op de knop Yes wordt geklikt

--- src/components/Todo/Dialogs/DialogDelete.vue
+++ src/components/Todo/Dialogs/DialogDelete.vue
@@ -6,14 +6,22 @@
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn color="gray darken-1" text @click="dialog = false"> No </v-btn>
-        <v-btn color="red darken-1" text @click="dialog = false"> Yes </v-btn>
+        <v-btn
+          color="red darken-1"
+          text
+          @click="$store.dispatch('deleteTask', task.id)"
+        >
+          Yes
+        </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["task"],
+};
 </script>
 
 <style></style>
