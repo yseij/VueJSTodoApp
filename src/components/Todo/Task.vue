d5a4b5f35df44fdebaa271b50cd26d8cf2bb23bb
yseij <r0790156@student.thomasmore.be>
2022-02-18 11:42:44 +0100

    Een taakmenu toevoegen

--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -16,9 +16,7 @@
           >
         </v-list-item-content>
         <v-list-item-action>
-          <v-btn icon @click.stop="dialogs.delete = true">
-            <v-icon color="primary lighten-1">mdi-delete</v-icon>
-          </v-btn>
+          <task-menu />
         </v-list-item-action>
       </template>
     </v-list-item>
@@ -45,6 +43,7 @@ export default {
   components: {
     "dialog-delete": require("@/components/Todo/Dialogs/DialogDelete.vue")
       .default,
+    "task-menu": require("@/components/Todo/TaskMenu.vue").default,
   },
 };
 </script>
