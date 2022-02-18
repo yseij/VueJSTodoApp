20bb25379c85e0222d910a126b5ef9b54353cac1
yseij <r0790156@student.thomasmore.be>
2022-02-18 19:49:15 +0100

    Filter de datum met date-fns

--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -18,7 +18,7 @@
         <v-list-item-action v-if="task.dueDate">
           <v-list-item-action-text>
             <v-icon small>mdi-calendar</v-icon>
-            {{ task.dueDate }}
+            {{ task.dueDate | niceDate }}
           </v-list-item-action-text>
         </v-list-item-action>
         <v-list-item-action>
@@ -32,8 +32,14 @@
 </template>
 
 <script>
+import { format } from "date-fns";
 export default {
   props: ["task"],
+  filters: {
+    niceDate(value) {
+      return format(new Date(value), "MMM d");
+    },
+  },
   components: {
     "task-menu": require("@/components/Todo/TaskMenu.vue").default,
   },
