51b508f237605db57b968553fc7e4a4cba336c2b
yseij <r0790156@student.thomasmore.be>
2022-02-18 12:14:13 +0100

    Taak verwijderen

--- src/components/Todo/TaskMenu.vue
+++ src/components/Todo/TaskMenu.vue
@@ -1,29 +1,40 @@
 <template>
-  <v-menu bottom left>
-    <template v-slot:activator="{ on, attrs }">
-      <v-btn color="primary" icon v-bind="attrs" v-on="on">
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </template>
+  <div>
+    <v-menu bottom left>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="primary" icon v-bind="attrs" v-on="on">
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
 
-    <v-list>
-      <v-list-item
-        v-for="(item, index) in items"
-        :key="index"
-        @click="handleClick(index)"
-      >
-        <v-list-item-icon>
-          <v-icon v-text="item.icon"></v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>{{ item.title }}</v-list-item-title>
-      </v-list-item>
-    </v-list>
-  </v-menu>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          @click="handleClick(index)"
+        >
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+    <dialog-delete
+      v-if="dialogs.delete"
+      :task="task"
+      @close="dialogs.delete = false"
+    />
+  </div>
 </template>
 
 <script>
 export default {
+  props: ["task"],
   data: () => ({
+    dialogs: {
+      delete: false,
+    },
     items: [
       {
         title: "Edit",
@@ -43,15 +54,19 @@ export default {
         title: "Delete",
         icon: "mdi-delete",
         click() {
-          console.log("Delete");
+          this.dialogs.delete = true;
         },
       },
     ],
   }),
   methods: {
     handleClick(index) {
-      this.items[index].click();
+      this.items[index].click.call(this);
     },
+  },
+  components: {
+    "dialog-delete": require("@/components/Todo/Dialogs/DialogDelete.vue")
+      .default,
   },
 };
 </script>
