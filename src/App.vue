822bbcfc48e7485b90fae576029c754d6a9b1b96
yseij <r0790156@student.thomasmore.be>
2022-02-18 21:00:40 +0100

    Verbeter de stijl

--- src/App.vue
+++ src/App.vue
@@ -38,10 +38,6 @@
       <v-spacer></v-spacer>
 
       <search />
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
--- src/components/Tools/Search.vue
+++ src/components/Tools/Search.vue
@@ -1,9 +1,21 @@
 <template>
-  <v-text-field label="Filled" filled></v-text-field>
+  <v-text-field
+    class="expanding-search mt-1"
+    placeholder="Search"
+    filled
+    dense
+    clearable
+    prepend-inner-icon="mdi-magnify"
+  ></v-text-field>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style lang="sass">
+.expanding-search
+  .v-input__slot
+    &:before, &after
+      border-color: transparant !important
+</style>
