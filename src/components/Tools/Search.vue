980007bf75be9668200f17889382faa69d424a54
yseij <r0790156@student.thomasmore.be>
2022-02-19 16:03:01 +0100

    Animatie toevoegen

--- src/components/Tools/Search.vue
+++ src/components/Tools/Search.vue
@@ -24,9 +24,12 @@ export default {
 
 <style lang="sass">
 .expanding-search
+  transition: max-width 0.3s
   .v-input__slot
     &:before, &after
       border-color: transparant !important
   &.closed
     max-width: 45px
+    .v-input__slot
+      background: rgb(0,0,0, 0) !important
 </style>
