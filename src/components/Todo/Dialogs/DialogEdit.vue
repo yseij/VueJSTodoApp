6b477b15599d7d9494ad40d16495cd2c6fdad553
yseij <r0790156@student.thomasmore.be>
2022-02-19 22:26:21 +0100

    Problemen met scrollen oplossen

--- src/components/Todo/Dialogs/DialogEdit.vue
+++ src/components/Todo/Dialogs/DialogEdit.vue
@@ -46,6 +46,7 @@ export default {
         };
         this.$store.dispatch("updateTaskTitle", payload);
         this.$emit("close");
+        this.$vuetify.goTo(0, { duration: 0 });
       }
     },
   },
